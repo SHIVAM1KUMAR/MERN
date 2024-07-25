da525b4245819008471f40fcf8a50c9d54a84d0a
SHIVAM1KUMAR <126109527+SHIVAM1KUMAR@users.noreply.github.com>
2024-07-25 20:30:13 +0530

    MERN_ALL_IN_ONE

--- CODE'S_MERN_ALL_IN_ONE/Express/index.js
+++ CODE'S_MERN_ALL_IN_ONE/Express/index.js
@@ -1,16 +1,64 @@
 console.log("Welcome to express.js");
 
 const express=require("express");
+const cookieParser = require('cookie-parser');
+
+
 const app=express();
+app.use(cookieParser());
 
-app.get("/users",(req,res)=>{
-    res.send("user List");
+app.get("/example",(req,res)=>{
+    const cookies=req.cookies;
+    console.log(cookies);
+    res.cookie("name","sk");
+    res.cookie("age","32");
+    res.send("Example cookies route");
 });
 
-app.get("/users/:id",(req,res)=>{
- console.log(req.params);
 
- res.send("user details");
+
+
+
+
+
+
+
+
+
+
+
+// const admin=express.Router();
+// const student=express.Router();
+
+// app.use("/admin",admin);
+// app.use("/student",student);
+
+// admin.get("/home",(req,res,next)=>{
+//     console.log(req.url);
+//     console.log(req.baseUrl);
+//     console.log(req.originalUrl);
+//     console.log(req.path);
+//     res.send("Admin home page route");
+// });
+
+// student.get("/home",(req,res,next)=>{
+//     console.log(req.baseUrl);
+//     console.log(req.originalUrl);
+//     console.log(req.path);
+//     res.send("Student home page route");
+// });
+
+// app.get("/home",(req,res,next)=>{
+//     console.log(req.baseUrl);
+//     console.log(req.originalUrl);
+//     console.log(req.path);
+//     res.send("Common home page route");
+// });
+
+
+
+app.listen(8000,()=>{
+    console.log("server is runningon port 8000");
 });
 
 
@@ -18,6 +66,32 @@ app.get("/users/:id",(req,res)=>{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/users",(req,res)=>{
+//     res.send("user List");
+// });
+
+// app.get("/users/:id",(req,res)=>{
+//  console.log(req.params);
+
+
+
+//  console.log(req.query);
+
+
+//  res.send("user details");
+// });
 
 
 
@@ -48,6 +122,3 @@ app.get("/users/:id",(req,res)=>{
 //     res.send("This is  delete method");
 // });
 
-app.listen(8000,()=>{
-    console.log("server is runningon port 8000");
-});
